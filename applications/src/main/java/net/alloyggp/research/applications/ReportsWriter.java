9135a227f248148d674dcf4a02b6f5c9f954ba4d
AlexLandau <alandau@cs.stanford.edu>
2017-08-20 18:16:15 -0700

    Write an index.html listing the generated reports.

--- applications/src/main/java/net/alloyggp/research/applications/ReportsWriter.java
+++ applications/src/main/java/net/alloyggp/research/applications/ReportsWriter.java
@@ -2,6 +2,7 @@ package net.alloyggp.research.applications;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.ggp.base.util.files.FileUtils;
@@ -15,7 +16,7 @@ public class ReportsWriter {
     public static void main(String[] args) throws IOException {
         List<Experiment> experiments = ExperimentRegistry.getExperiments();
 
-        File reportsDir = Files.getReportsDirectory();
+        File reportsDir = Files.getReportsDirectory().getCanonicalFile();
 
         for (Experiment experiment : experiments) {
             List<MatchResult> results = MatchResults.loadAllResults(experiment.getName());
@@ -25,7 +26,10 @@ public class ReportsWriter {
             FileUtils.writeStringToFile(reportFile, html);
         }
 
-        // TODO: Write an index.html with links to the different experiment reports
+        File indexFile = new File(reportsDir, "index.html");
+        FileUtils.writeStringToFile(indexFile, writeIndexHtml(experiments));
+
+        System.out.println("Reports available at: " + indexFile.toURI());
     }
 
     private static String getExperimentFilename(String name) {
@@ -36,5 +40,30 @@ public class ReportsWriter {
             throw new IllegalArgumentException("Shouldn't have an experiment named 'index'!");
         }
         return cleanedName + ".html";
+    }
+
+    private static String writeIndexHtml(List<Experiment> experiments) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<html><head><title>Game AI Research - Results</title></head>\n");
+        sb.append("<body>\n");
+
+        sb.append("<p>");
+        sb.append("Reports last updated: " + LocalDateTime.now());
+        sb.append("</p>\n");
+
+        sb.append("<ul>\n");
+        for (Experiment experiment : experiments) {
+            String filename = getExperimentFilename(experiment.getName());
+            sb.append("  <li>");
+            sb.append("<a href=\"").append(filename).append("\">");
+            sb.append(experiment.getName());
+            sb.append("</a></li>\n");
+        }
+        sb.append("</ul>\n");
+
+        sb.append("</body></html>\n");
+
+        return sb.toString();
     }
 }
