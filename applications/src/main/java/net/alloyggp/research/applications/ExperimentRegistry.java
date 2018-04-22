2042e760f42b5585eabb9f734661828107140b47
AlexLandau <alandau@cs.stanford.edu>
2018-04-22 16:49:23 -0700

    Add more focused C_p comparison tests

--- applications/src/main/java/net/alloyggp/research/applications/ExperimentRegistry.java
+++ applications/src/main/java/net/alloyggp/research/applications/ExperimentRegistry.java
@@ -83,6 +83,48 @@ public class ExperimentRegistry {
 
                 // Sample A/B test experiment
                 new ABTestExperiment(
+                        "TestUCT200CpExperiment",
+                        ImmutableList.of(
+                                "UCTOneNodeExpansion:iterationCount=200:c_p=0.353553",
+                                "UCTOneNodeExpansion:iterationCount=200:c_p=0.707107",
+                                "UCTOneNodeExpansion:iterationCount=200:c_p=1.414214",
+                                "UCTOneNodeExpansion:iterationCount=200:c_p=2.828427"
+                                ),
+                        ImmutableList.of(Game.TIC_TAC_TOE,
+                                Game.BREAKTHROUGH,
+                                Game.DOTS_AND_BOXES,
+                                Game.DOTS_AND_BOXES_MISERE,
+                                Game.ENGLISH_DRAUGHTS,
+                                Game.CONNECT_FOUR_8x6,
+                                Game.CONNECT_FOUR_9x6,
+                                Game.PENTAGO,
+                                Game.REVERSI,
+                                Game.CEPHALOPOD_3x3),
+                        250),
+
+                // Sample A/B test experiment
+                new ABTestExperiment(
+                        "TestUCTWinsFirst200CpExperiment",
+                        ImmutableList.of(
+                                "UCTWinsFirst:iterationCount=200:c_p=0.353553",
+                                "UCTWinsFirst:iterationCount=200:c_p=0.707107",
+                                "UCTWinsFirst:iterationCount=200:c_p=1.414214",
+                                "UCTWinsFirst:iterationCount=200:c_p=2.828427"
+                                ),
+                        ImmutableList.of(Game.TIC_TAC_TOE,
+                                Game.BREAKTHROUGH,
+                                Game.DOTS_AND_BOXES,
+                                Game.DOTS_AND_BOXES_MISERE,
+                                Game.ENGLISH_DRAUGHTS,
+                                Game.CONNECT_FOUR_8x6,
+                                Game.CONNECT_FOUR_9x6,
+                                Game.PENTAGO,
+                                Game.REVERSI,
+                                Game.CEPHALOPOD_3x3),
+                        250),
+
+                // Sample A/B test experiment
+                new ABTestExperiment(
                         "TestUCTWinsFirstABExperiment",
                         ImmutableList.of(
                                 "UCTRecordAllNodes:iterationCount=20",
